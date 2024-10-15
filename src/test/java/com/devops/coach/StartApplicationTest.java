d8ae0788d51d4e29a03aa9822432f6656e81b678
saud4567 <138786824+saud4567@users.noreply.github.com>
2024-10-15 11:34:29 +0530

    Update StartApplicationTest.java

--- src/test/java/com/devops/coach/StartApplicationTest.java
+++ src/test/java/com/devops/coach/StartApplicationTest.java
@@ -1,5 +1,4 @@
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertTrue;  // Add this line for assertTrue
 import java.time.LocalDate;
 import org.junit.Test;
 import org.springframework.ui.Model;
